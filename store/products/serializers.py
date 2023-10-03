c7e722033c06fb631a72767ab0e8fa5361f5aab3
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2023-10-03 19:20:45 +0300

    API Получение токена, (создание,удаление,обновления продуктов)

--- store/products/serializers.py
+++ store/products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,3 +9,11 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
+
+
+class BasketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Basket
+        fields = ('id', 'product', 'quantity', 'created_timestamp')
+        read_only_fields = ('created_timestamp',)
--- store/store/settings.py
+++ store/store/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'django_extensions',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'products',
     'users',
@@ -252,5 +253,10 @@ STRIPE_WEBHOOK_SECRET = env('STRIPE_WEBHOOK_SECRET')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 3,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
 }
